2f7f8f25e782d7982b701eed59e2163fbede4179
Juan Batty <jmbl1685@hotmail.com>
2018-02-08 22:27:08 -0500

    JB - AppExample.ExtensionUtility

---
+++ AppExample.ExtensionUtility/ExtensionUtility.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using System;
+
+namespace AppExample
+{
+    public static class ExtensionUtility
+    {
+        public static ObjectId ParseID(string id)
+        {
+            try
+            {
+                return ObjectId.Parse(id);
+            }
+            catch (Exception e)
+            {
+                return ObjectId.Empty;
+            }
+        }
+
+        public static ObjectId GenerateID()
+        {
+            return ObjectId.GenerateNewId();
+        }
+
+    }
+}
