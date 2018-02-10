0483910ac5b2321bff052c5f3be895084239a6f3
Juan Batty <jmbl1685@hotmail.com>
2018-02-09 22:29:08 -0500

    JB - AppExample.Repository

---
+++ AppExample.Repository/IPlayerRepository.cs
@@ -0,0 +1,18 @@
+﻿using AppExample.Entities;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppExample.Repository
+{
+    public interface IPlayerRepository
+    {
+        Task<Player> CreatePlayer(Player player);
+        Task<Player> ReadPlayer(ObjectId playerID);
+        Task<Player> UpdatePlayer(ObjectId playerID, Player player);
+        Task<Player> DeletePlayer(ObjectId playerID);
+        Tuple<List<Player>,long> ReadAllPlayer(int? index = 0 ,int limit= 5);
+    }
+}
---
+++ AppExample.Repository/RepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿using AppExample.Repository.impl;
+using System;
+
+namespace AppExample.Repository
+{
+    public static class RepositoryFactory
+    {
+        public static PlayerRepository GetPlayerRepository()
+        {
+            return new PlayerRepository();
+        }
+    }
+}
